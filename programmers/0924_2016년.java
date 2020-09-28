31ac65976e19ff739ed3678132b534dd79d8f11e
Youngjo Cha <chyoungjj329@gmail.com>
2020-09-28 13:15:57 +0900

    Create 0924_2016년.java

---
+++ programmers/0924_2016년.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String solution(int a, int b) {
+        String answer = "";
+        String[] today = {"THU", "FRI", "SAT", "SUN", "MON", "TUE", "WED"};
+        int day = 0;
+
+        for(int i = 1; i < a; i++){
+            switch (i){
+                case 1:
+                case 3:
+                case 5:
+                case 7:
+                case 8:
+                case 10:
+                case 12:
+                    day += 31;
+                    break;
+                case 4:
+                case 6:
+                case 9:
+                case 11:
+                    day += 30;
+                    break;
+                case 2:
+                    day += 29;
+                    break;
+            }
+        }
+        day += b;
+
+        answer = today[day % 7] ;
+
+        return answer;
+    }
+}
